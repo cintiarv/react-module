40a4abf5d8b66184a7e0c718805925fec503241a
Cintia Ruiz <cintiaruizv@gmail.com>
2022-10-27 15:03:04 -0500

    fixing

--- homework/create-component/src/App.jsx
+++ homework/create-component/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import { CardUpdated } from './component/Card'
+import { Card } from './component/Card'
+
+const userInfo = {
+  profImg:'src/assets/image-victor.jpg',
+  name:'Victor Crest',
+  age : '26',
+  location:'London',
+  followersAmount:'80',
+  likesAmount:'803',
+  photosAmount:'1.4'
+}
 
 function App () {
   return (
-    <CardUpdated />
+    <Card 
+    profImg={userInfo.profImg}
+        name={userInfo.name}
+        age={userInfo.age}
+        location={userInfo.location}
+        followersAmount={userInfo.followersAmount}
+        likesAmount={userInfo.likesAmount}
+        photosAmount={userInfo.photosAmount}
+    />
 
   )
 }
